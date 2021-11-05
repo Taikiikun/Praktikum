00785a56d821d4360e0b426edf4d355791baa2e7
Taikiikun <92316485+Taikiikun@users.noreply.github.com>
2021-11-05 13:46:37 +0100

    Create hallo.c

---
+++ hallo.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+        printf("Hallo.\n");
+        printf("Willkommen in AP1.\n");
+        printf("Hoffentlich sehen wir uns bald live.\n");
+        return 0;
+}
