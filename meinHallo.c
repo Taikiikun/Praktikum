9a660e58da30ccdd4e2933dad397a2251b1f864f
Taikiikun <92316485+Taikiikun@users.noreply.github.com>
2021-11-05 13:47:07 +0100

    Create meinHallo.c

---
+++ meinHallo.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+        printf("Hi folks.\n");
+        printf("My Name is Elon Musk.\n");
+        printf("Great to meet you here in Gummersbach!\n");
+        return 0;
+}
