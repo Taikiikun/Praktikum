daa921cc3e9845211209a01fc4f332cb41c7af26
Taikiikun <92316485+Taikiikun@users.noreply.github.com>
2021-11-07 22:00:28 +0100

    Create primzahlpuefer.c

---
+++ primzahlpuefer.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+  int zahl = 0;
+  int counter = 3;
+  
+  printf("Programm: Ist n eine Primezahl?\n\n");
+  printf("Bitte geben Sie für n eine Zahl ein:\n");
+  scanf("%i", &zahl);
+  
+  if(zahl < 2 && zahl > -2)
+    printf("JA!\n");
+  for(counter = 3; counter < zahl; counter++)
+  {
+    if(zahl % counter == 0)
+    {  
+      printf("NEIN\n");
+      return 0;
+    }
+  }
+  printf("JA\n");
+  return 0;
+}
