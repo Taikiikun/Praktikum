f93d5f91c81986265579aa7b307e864c5b225fb5
Taikiikun <92316485+Taikiikun@users.noreply.github.com>
2021-11-08 16:14:42 +0100

    Update warmup1.c

--- warmup1.c
+++ warmup1.c
@@ -6,7 +6,7 @@ int main()
 {
     int x, y, sum;
     
-    printf("Programm: Berechne x + y\n\n");
+    printf("Berechne x + y\n\n");
     printf("Bitte Wert für x eingeben: ");
     scanf("%i", &x);
     printf("Bitte Wert für y eingeben: ");
