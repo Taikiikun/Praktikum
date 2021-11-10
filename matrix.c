d35e4c86f9830151a0e6ff52a4f0e9117f5f3ce5
Taikiikun <92316485+Taikiikun@users.noreply.github.com>
2021-11-10 23:14:02 +0100

    Update matrix.c

--- matrix.c
+++ matrix.c
@@ -1,23 +1,25 @@
+#include <stdio.h>
+
 int main()
 {
     const int zeile = 5;
     const int spalte = 5;
-    int matrix[5][5];
+    char matrix[5][5];
     int n, m;
     char zeichen = 'X';
     
     printf("Die Matrix sieht so aus: \n\n");
-    for(n = 0; n < zeile; n++){
-        for(m = 0; m < spalte; m++){
+    for(n = 0; n <= zeile; n++){
+        for(m = 0; m <= spalte; m++){
             matrix[n][m] = zeichen;
         }
     }
     for(n = 0; n < 5; n++){
         for(m = 0; m < 5; m++){
             //printf("\n");
-            printf("%c \t ",matrix[n][m]);
+            //printf("%c \t ",matrix[n][m]);
         }
-    //printf("%c \t\n ",matrix[n][m]);
+    printf("%c \n",matrix[n][m]);
     }
 
     return 0;
